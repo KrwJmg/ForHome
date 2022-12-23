f7fd30abea57e91201bc090d572c846ecb08b3cf
KrwJmg <flook_11323@hotmail.com>
2022-12-23 11:17:00 +0700

    Test

---
+++ src/Ex1.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class Ex1 {
+
+	public static void main(String[] args) {
+	     JOptionPane.showMessageDialog(null, "Error!!\n"
+	     		+ "Please,press any key  to close it!!","WARNING_MESSAGE", 0);
+
+	}
+
+}
